ca79c643d4af0072f12a1a503d152b3a229e188a
Max Orlovsky <max.orlovsky@betssongroup.com>
2017-05-09 19:00:54 +0200

    Add some sound commands

---
+++ public/wallc/checker.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const key = 'AIzaSyD9-k50LdAnOJxGcJpjji-gkRkX9BmYoAE';
+const secret = 'test';
+const calendars = ['max.orlovsky@gmail.com', 'anya.orlovsky@gmail.com'];
+const vars = {
+    calendarsUpdate: {
+        "max.orlovsky@gmail.com": 0,
+        "anya.orlovsky@gmail.com": 0
+    }
+}
+
+setInterval(() => {
+    for (calendar of calendars) {
+        checkCalendar(calendar);
+    }
+
+    console.log(vars);
+}, 15000);
+
+function checkCalendar(calendarId) {
+    fetch('https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events?key=' + key)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        if (vars.calendarsUpdate[calendarId] === 0) {
+            vars.calendarsUpdate[calendarId] = json.updated;
+        }
+        //else if ()
+    });
+}
